c66d36af5384400541cf5e110f40b38927f6f7df
Adam Grossi <adam.m.grossi@gmail.com>
2021-03-24 11:59:52 -0400

    Custom useCloseWithEscapeKey updates: - Remove unnecessary parameter for close function - Remove unnecessary event parameter

--- js/react/hooks/useCloseWIthEscapeKey.ts
+++ js/react/hooks/useCloseWIthEscapeKey.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 export const useCloseWithEscapeKey = (keyDownTargetRef: React.MutableRefObject<HTMLElement | null>, closeCallback: () => void, open: boolean) => {
-  const handleEscapeKeyPress = (event: KeyboardEvent, closeCallback: () => void) => {
+  const handleEscapeKeyPress = (event: KeyboardEvent) => {
     if (event.key === `Escape`) {
       return closeCallback();
     }
@@ -9,10 +9,10 @@ export const useCloseWithEscapeKey = (keyDownTargetRef: React.MutableRefObject<H
   useEffect(() => {
     const ref = keyDownTargetRef.current;
     if (open) {
-      ref?.addEventListener(`keydown`, (event: KeyboardEvent) => handleEscapeKeyPress(event, closeCallback));
+      ref?.addEventListener(`keydown`, handleEscapeKeyPress);
     }
     return () => {
-      ref?.removeEventListener(`keydown`, (event: KeyboardEvent) => handleEscapeKeyPress(event, closeCallback));
+      ref?.removeEventListener(`keydown`, handleEscapeKeyPress);
     }
   });
 }
