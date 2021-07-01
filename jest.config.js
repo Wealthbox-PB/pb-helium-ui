32fb8955563848bbe863ed40ac5f4a38030e5570
Adam Grossi <adam.m.grossi@gmail.com>
2021-07-01 11:07:52 -0400

    Configure jest and typescript for testing/prod environments

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   moduleFileExtensions: [`js`, `json`, `jsx`, `ts`, `tsx`],
-  moduleDirectories: [`node_modules`, `<rootDir>/js`],
-  roots: [`<rootDir>/js/spec`],
-  setupFiles: [`<rootDir>/js/spec/jest_config_setup.js`],
+  moduleDirectories: [`node_modules`],
+  roots: [`<rootDir>/ts/spec`],
+  setupFiles: [`<rootDir>/ts/spec/jest_config_setup.js`],
   snapshotSerializers: [`enzyme-to-json/serializer`],
   testMatch: [`**/*.+(ts|tsx)`, `**/?(*.)+(spec|test).+(ts|tsx)`],
   transform: {
