631011b4463bb5b4732faef3e92996505b9b96b9
Adam Grossi <adam.m.grossi@gmail.com>
2022-01-14 15:22:21 -0500

    Upgrade to stylelint-14, configure packages as needed, and fix failures

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,33 +2,36 @@
 // any updates or enhancements made to that file should be
 // reviewed and considered here as well.
 
-"use strict";
+'use strict';
 
 module.exports = {
-  extends: [`stylelint-config-standard`, `stylelint-config-idiomatic-order`],
+  extends: [`stylelint-config-standard-scss`, `stylelint-config-idiomatic-order`],
   plugins: [`stylelint-order`],
   rules: {
-    "at-rule-no-unknown": [
+    'alpha-value-notation': null, // Opacity using percentages is not supported in Safari
+    'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: [
-          `at-root`,
-          `each`,
-          `else`,
-          `extend`,
-          `for`,
-          `function`,
-          `if`,
-          `include`,
-          `mixin`,
-          `return`,
-        ],
+        ignoreAtRules: [`at-root`, `each`, `extend`, `for`, `if`, `else`, `function`, `return`, `include`, `mixin`],
       },
     ],
-    "block-opening-brace-newline-after": [`always`],
-    "block-closing-brace-newline-before": [`always`],
-    "no-descending-specificity": null,
-    "selector-type-no-unknown": null,
-    "string-quotes": `double`,
+    'max-line-length': [
+      120, // Default set in stylelint-config-standard package
+      {
+        ignore: [`comments`],
+        // ignorePattern: [`/(https?://([-\\w\\.]+)+(:\\d+)?(/([\\w/_\\.]*(\\?\\S+)?)?)?)/`],
+      },
+    ],
+    'color-function-notation': null, // Revisit this rule after upgrading to Dart Sass
+    'no-descending-specificity': null,
+    'no-empty-first-line': null,
+    'no-invalid-position-at-import-rule': null, // Revisit this rule after upgrading to Dart Sass
+    'scss/at-extend-no-missing-placeholder': null,
+    'scss/double-slash-comment-empty-line-before': null,
+    'scss/double-slash-comment-whitespace-inside': null,
+    'scss/no-global-function-names': null, // Revisit this rule after upgrading to Dart Sass
+    'selector-class-pattern': 'h-[a-z]+', // helium-ui specific configuration
+    'selector-id-pattern': 'h-[a-z]+', // helium-ui specific configuration
+    'selector-type-no-unknown': null,
   },
 };
