ea51e88e47788f58f70ac1581f29b7d7bd0ee573
Michal Kopanski <info@michalkopanski.com>
2023-04-10 16:31:04 -0400

    Update  output

--- dist/js/components/Badge.js
+++ dist/js/components/Badge.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 export var Badge = function (_a) {
     var _b;
     var children = _a.children, className = _a.className, contentClassName = _a.contentClassName, iconClassName = _a.iconClassName, _c = _a.scale, scale = _c === void 0 ? "base" : _c, _d = _a.variant, variant = _d === void 0 ? "info" : _d;
-    return (React.createElement("span", { className: classNames("h-badge h-badge--" + variant, (_b = {}, _b["h-badge--" + scale] = scale !== "base", _b), className), "data-testid": "h-badge" },
+    return (React.createElement("span", { className: classNames("h-badge h-badge--".concat(variant), (_b = {}, _b["h-badge--".concat(scale)] = scale !== "base", _b), className), "data-testid": "h-badge" },
         iconClassName ? (React.createElement("span", { className: classNames("h-badge__icon", iconClassName), "aria-hidden": "true", "data-testid": "h-badge__icon" })) : null,
         React.createElement("span", { className: classNames("h-badge__content", contentClassName), "data-testid": "h-badge__content" }, children)));
 };
