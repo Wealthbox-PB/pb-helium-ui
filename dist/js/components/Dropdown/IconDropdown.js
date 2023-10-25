7e7035a75b3ec5f5e7dcafb09a2445060274647c
Jeffrey Bondorew <jeffrey@bondorew.com>
2023-10-25 11:25:32 -0400

    extend IconDropdown props to include Dropdown props

--- dist/js/components/Dropdown/IconDropdown.js
+++ dist/js/components/Dropdown/IconDropdown.js
@@ -25,12 +25,12 @@ import { Dropdown } from './Dropdown';
 import { Button } from '../Button';
 import classNames from 'classnames';
 var IconDropdown = function (_a) {
-    var ariaLabel = _a.ariaLabel, className = _a.className, buttonId = _a.buttonId, children = _a.children, iconClassName = _a.iconClassName, _b = _a.iconName, iconName = _b === void 0 ? "dots" : _b, _c = _a.placement, placement = _c === void 0 ? "bottom-end" : _c, _d = _a.size, size = _d === void 0 ? "xs" : _d, _e = _a.variant, variant = _e === void 0 ? "border-hover" : _e;
-    return (React.createElement(Dropdown, { renderOpener: function (_a) {
+    var ariaLabel = _a.ariaLabel, className = _a.className, buttonId = _a.buttonId, children = _a.children, iconClassName = _a.iconClassName, _b = _a.iconName, iconName = _b === void 0 ? "dots" : _b, _c = _a.size, size = _c === void 0 ? "xs" : _c, _d = _a.variant, variant = _d === void 0 ? "border-hover" : _d, rest = __rest(_a, ["ariaLabel", "className", "buttonId", "children", "iconClassName", "iconName", "size", "variant"]);
+    return (React.createElement(Dropdown, __assign({ renderOpener: function (_a) {
             var ref = _a.ref, props = __rest(_a, ["ref"]);
             return (React.createElement(Button, __assign({ variant: variant, size: size, ref: ref, "aria-label": ariaLabel, square: true, id: buttonId, className: className }, props),
                 React.createElement("span", { className: classNames("h-icon-".concat(iconName), iconClassName) })));
-        }, placement: placement }, children));
+        } }, rest), children));
 };
 export { IconDropdown };
 //# sourceMappingURL=IconDropdown.js.map
--- ts/components/Dropdown/IconDropdown.tsx
+++ ts/components/Dropdown/IconDropdown.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import { Dropdown } from './Dropdown';
-import type { Placement } from '@floating-ui/react';
 import { Button, ButtonSize, ButtonVariant } from '../Button';
 import classNames from 'classnames';
 import type { Icons } from '../../types/icons';
 
-interface IconDropdownProps {
+interface IconDropdownProps extends Omit<ComponentProps<typeof Dropdown>, `renderOpener`> {
   ariaLabel?: string;
   className?: string;
   buttonId?: string;
   iconClassName?: string;
   iconName?: Icons;
-  placement?: Placement;
   children: JSX.Element[] | JSX.Element;
   variant?: ButtonVariant;
   size?: ButtonSize;
@@ -24,9 +22,9 @@ const IconDropdown = ({
   children,
   iconClassName,
   iconName = `dots`,
-  placement = `bottom-end`,
   size = `xs`,
   variant = `border-hover`,
+  ...rest
 }: IconDropdownProps) => (
   <Dropdown
     renderOpener={({ ref, ...props }) => (
@@ -43,7 +41,7 @@ const IconDropdown = ({
         <span className={classNames(`h-icon-${iconName}`, iconClassName)}></span>
       </Button>
     )}
-    placement={placement}
+    {...rest}
   >
     {children}
   </Dropdown>
