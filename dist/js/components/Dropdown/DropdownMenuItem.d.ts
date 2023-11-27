64d19454f92ed31f3b19d63c1ee1fbc9c82aa8f4
Michal Kopanski <info@michalkopanski.com>
2023-11-27 14:28:50 -0500

    Remove DropdownMenuItem component

--- dist/js/components/Dropdown/DropdownMenuItem.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface DropdownMenuItemProps {
-    label: string;
-    children: JSX.Element;
-}
-export declare const DropdownMenuItem: ({ label, ...props }: DropdownMenuItemProps) => React.JSX.Element;
-export {};
