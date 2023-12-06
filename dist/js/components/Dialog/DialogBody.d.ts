ad623dd6390a0a4c1738fb690aea92c64c878afd
Michal Kopanski <info@michalkopanski.com>
2023-12-05 21:28:50 -0500

    Update dist files

--- dist/js/components/Dialog/DialogBody.d.ts
+++ dist/js/components/Dialog/DialogBody.d.ts
@@ -1,8 +1,11 @@
 import React from 'react';
 interface DialogBodyProps {
-    ariaDescriptionSelector?: string;
-    bodyClassName?: string;
+    /** Content for the dialog body. */
     children: string | JSX.Element[] | JSX.Element;
+    /** Used to set the the id attribute of the dialog body */
+    ariaDescriptionSelector?: string;
+    /** Adds class names to the dialog body container. */
+    bodyClassName?: string;
 }
 declare const DialogBody: ({ bodyClassName, ariaDescriptionSelector, children }: DialogBodyProps) => React.JSX.Element;
 export { DialogBody };
--- dist/js/components/Dropdown/DropdownMenuLink.d.ts
+++ dist/js/components/Dropdown/DropdownMenuLink.d.ts
@@ -1,13 +1,21 @@
 import React, { AnchorHTMLAttributes, MouseEvent } from 'react';
 import { Icons } from '../../types/icons';
 interface DropdownMenuLinkProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, `className` | `onClick`> {
-    children?: JSX.Element[] | JSX.Element;
-    className?: string;
-    iconName?: Icons;
+    /** Text used for the dropdown menu item, and for the typeahead matching. Setting children will override
+     * this content. */
     label: string;
+    /** Content for the dropdown menu item button. Setting children will override the default behavior of the
+     * label and icon being displayed. */
+    children?: JSX.Element[] | JSX.Element;
+    /** Adds class names to the dropdown menu item. */
+    className?: string;
+    /** Displays an icon to the left of the label. Setting children will override this content. */
+    iconName?: Icons;
+    /** Adds class names to the dropdown menu item link. */
     linkClassName?: string;
+    /** Callback function when the dropdown menu item is clicked. */
     onClick?: (e: MouseEvent<HTMLElement>) => void;
-    useSelect?: string;
+    /** Controls the variant of the dropdown menu item. */
     variant?: `default` | `negative`;
 }
 export declare const DropdownMenuLink: ({ children, label, iconName, variant, className, linkClassName, onClick, ...props }: DropdownMenuLinkProps) => React.JSX.Element;
