76efe7e996370fa6847f65518d752a688ce7c705
Michal Kopanski <info@michalkopanski.com>
2024-01-05 08:58:33 -0500

    Fix specs

--- spec/Spinner.test.tsx
+++ spec/Spinner.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { HeliumSpinner, Spinner } from '../ts/components/Spinner/Spinner';
+import { HeliumSpinner } from '../ts/components/Spinner/HeliumSpinner';
+import { Spinner } from '../ts/components/Spinner/Spinner';
 import { base64SpinnerImg, base64SpinnerImgDark } from '../ts/components/Spinner/base_64_spinner_img';
 
 describe(`<Spinner />`, () => {
