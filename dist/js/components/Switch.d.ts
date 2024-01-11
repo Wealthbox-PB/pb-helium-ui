9b65c27277583b039416b206eec209eb4dd0ff69
Michal Kopanski <info@michalkopanski.com>
2024-01-11 17:29:52 -0500

    Add prop descriptions

--- dist/js/components/Switch.d.ts
+++ dist/js/components/Switch.d.ts
@@ -1,14 +1,20 @@
 import React from 'react';
 interface SwitchProps {
+    /** Aria label for the switch. Defaults to On/Off depending on the switch's checked value */
     ariaLabel?: string;
+    /** Controls whether the switch is checked or not. */
     checked?: boolean;
     /**
      * @deprecated `defaultValue` prop is deprecated. Please use the `checked` prop instead.
      */
     defaultValue?: boolean;
+    /** Controls whether the switch is disabled or not. */
     disabled?: boolean;
+    /** Name for the hidden input. */
     name?: string;
+    /** Callback function when the switch is changed. */
     onChange?: (value: boolean) => void;
+    /** Controls the color of the switch. */
     variant?: `primary` | `positive`;
 }
 export declare const Switch: ({ ariaLabel, checked, defaultValue, disabled, name, onChange, variant, }: SwitchProps) => React.JSX.Element;
--- ts/components/Switch.tsx
+++ ts/components/Switch.tsx
@@ -3,15 +3,21 @@ import classNames from 'classnames';
 import { Switch as HeadlessUiSwitch } from '@headlessui/react';
 
 interface SwitchProps {
+  /** Aria label for the switch. Defaults to On/Off depending on the switch's checked value */
   ariaLabel?: string;
+  /** Controls whether the switch is checked or not. */
   checked?: boolean;
   /**
    * @deprecated `defaultValue` prop is deprecated. Please use the `checked` prop instead.
    */
   defaultValue?: boolean;
+  /** Controls whether the switch is disabled or not. */
   disabled?: boolean;
+  /** Name for the hidden input. */
   name?: string;
+  /** Callback function when the switch is changed. */
   onChange?: (value: boolean) => void;
+  /** Controls the color of the switch. */
   variant?: `primary` | `positive`;
 }
 
