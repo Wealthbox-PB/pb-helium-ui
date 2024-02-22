bf06c6f80594728a5b9d83f811adc8355bbf9d51
Jeffrey Bondorew <jeffrey@bondorew.com>
2024-02-22 09:18:20 -0500

    Add comments to Select and SelectMenuButton Props * Remove unused useSelect prop

--- dist/js/components/Select/Select.d.ts
+++ dist/js/components/Select/Select.d.ts
@@ -5,16 +5,30 @@ interface RenderOpenerProps {
     selectedLabel: string | null;
 }
 interface SelectProps {
+    /** Content for the dropdown. */
     children: JSX.Element | JSX.Element[];
-    closeOnSelect?: boolean;
-    flip?: boolean;
-    minHeight?: number;
-    maxHeight?: number;
-    height?: string;
-    initialSelectedValue?: string | null;
-    initialSelectedIndex?: number | null;
-    placement?: Placement;
+    /** The dropdown opener element. Use the destructured "ref" and "...props" to spread them onto the
+     * opener element */
     renderOpener: (props: RenderOpenerProps) => JSX.Element;
+    /** Controls whether the dropdown should be close when an item is selected */
+    closeOnSelect?: boolean;
+    /** Controls whether the dropdown should flip it's orientation based on the available space in the browser
+     * window */
+    flip?: boolean;
+    /** Controls the height of the dropdown. */
+    height?: string;
+    /** Controls the initial selected value. */
+    initialSelectedValue?: string | null;
+    /** Controls the initial selected index. */
+    initialSelectedIndex?: number | null;
+    /** Controls the maximum height of the dropdown. */
+    maxHeight?: number;
+    /** Controls the minimum height of the dropdown. */
+    minHeight?: number;
+    /** Controls the placement of the dropdown. */
+    placement?: Placement;
+    /** Controls the width of the dropdown. "full" width makes the dropdown the same width as the opener
+     * element. */
     width?: `auto` | `full` | number;
 }
 declare const Select: ({ children, closeOnSelect, flip: flipProp, initialSelectedValue, initialSelectedIndex, minHeight, placement, renderOpener, width, maxHeight, height, }: SelectProps) => React.JSX.Element;
--- dist/js/components/Select/SelectMenuButton.d.ts
+++ dist/js/components/Select/SelectMenuButton.d.ts
@@ -1,13 +1,21 @@
 import React, { ButtonHTMLAttributes, MouseEvent } from 'react';
 import { Icons } from '../../types/icons';
 interface SelectMenuButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, `className` | `onClick`> {
-    buttonClassName?: string;
-    children?: JSX.Element[] | JSX.Element;
-    className?: string;
-    iconName?: Icons;
-    label: string;
+    /** Callback function when the dropdown menu item is clicked. */
     onClick: (e?: MouseEvent<HTMLElement>) => void;
-    useSelect?: string;
+    /** Text used for the dropdown menu item, and for the typeahead matching. Setting children will override
+     * this content. */
+    label: string;
+    /** Adds class names to the dropdown menu item button. */
+    buttonClassName?: string;
+    /** Content for the select menu item button. Setting children will override the default behavior of the
+     * label, select checkmark, and icon being displayed. */
+    children?: JSX.Element[] | JSX.Element;
+    /** Adds class names to the dropdown menu item. */
+    className?: string;
+    /** Displays an icon to the left of the label. Setting children will override this content. */
+    iconName?: Icons;
+    /** Controls the variant of the dropdown menu item. */
     variant?: `default` | `negative`;
 }
 export declare const SelectMenuButton: ({ buttonClassName, children, className, iconName, label, onClick, variant, ...props }: SelectMenuButtonProps) => React.JSX.Element;
