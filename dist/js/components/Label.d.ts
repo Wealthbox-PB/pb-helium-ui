b49ca29c6c572005f7b7762bb0c5dde1da297560
Jeffrey Bondorew <jeffrey@bondorew.com>
2024-02-26 10:06:10 -0500

    Add prop comments for Label, Interstitial, and Button

--- dist/js/components/Label.d.ts
+++ dist/js/components/Label.d.ts
@@ -1,7 +1,10 @@
 import React from 'react';
 interface LabelProps {
+    /** Content for the label. */
     children: string | JSX.Element | JSX.Element[];
+    /** Sets the `htmlFor` of the label. */
     htmlFor?: string;
+    /** Adds class names to the label. */
     labelClassName?: string;
 }
 declare const Label: ({ children, htmlFor, labelClassName }: LabelProps) => React.JSX.Element;
--- ts/components/Label.tsx
+++ ts/components/Label.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import classNames from 'classnames';
 
 interface LabelProps {
+  /** Content for the label. */
   children: string | JSX.Element | JSX.Element[];
+  /** Sets the `htmlFor` of the label. */
   htmlFor?: string;
+  /** Adds class names to the label. */
   labelClassName?: string;
 }
 
