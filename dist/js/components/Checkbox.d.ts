909f7ec3621fcda9783a8424a3e2a6ed0bdf5e3c
Jeffrey Bondorew <jeffrey@bondorew.com>
2024-05-10 15:07:23 -0400

    Add prop comments

--- dist/js/components/Checkbox.d.ts
+++ dist/js/components/Checkbox.d.ts
@@ -1,11 +1,18 @@
 import React, { InputHTMLAttributes } from 'react';
 interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, `className` | `size` | `type`> {
+    /** Set the checkbox state. */
     checked: boolean;
+    /** Set the checkbox wrapping html label class name. */
     className?: string;
+    /** Set the checkbox visual state to indeterminate. */
     indeterminate?: boolean;
+    /** Set the checkbox input class name. */
     inputClassName?: string;
+    /** Set the checkbox text label content. */
     label?: string;
+    /** Set the checkbox text label class name. */
     labelClassName?: string;
+    /** Controls the checkbox size. */
     size?: `sm` | `md` | `lg`;
 }
 declare const CheckboxRef: React.ForwardRefExoticComponent<CheckboxProps & React.RefAttributes<unknown>>;
--- ts/components/Checkbox.tsx
+++ ts/components/Checkbox.tsx
@@ -2,12 +2,19 @@ import React, { forwardRef, InputHTMLAttributes } from 'react';
 import classNames from 'classnames';
 
 interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, `className` | `size` | `type`> {
+  /** Set the checkbox state. */
   checked: boolean;
+  /** Set the checkbox wrapping html label class name. */
   className?: string;
+  /** Set the checkbox visual state to indeterminate. */
   indeterminate?: boolean;
+  /** Set the checkbox input class name. */
   inputClassName?: string;
+  /** Set the checkbox text label content. */
   label?: string;
+  /** Set the checkbox text label class name. */
   labelClassName?: string;
+  /** Controls the checkbox size. */
   size?: `sm` | `md` | `lg`;
 }
 
