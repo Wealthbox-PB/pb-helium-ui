c5f0642004e8525001a73286fe378f4b20cea6be
Randy Glasgow <randy.glasgow@wealthbox.com>
2025-01-06 14:40:38 -0700

    Update .storybook/manager.ts
    Co-authored-by: Paul Barry <paul.barry@wealthbox.com>

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,10 +1,14 @@
 import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming';
 
+const THEME_BASE = 'light';
+const TITLE = 'Helium UI;
+const BACKGROUND_COLOR = '#106ed405';
+
 const theme = create({
-  base: 'light',
-  brandTitle: 'Helium UI',
-  appBg: '#106ed405',
+  base: THEME_BASE,
+  brandTitle: TITLE,
+  appBg: BACKGROUND_COLOR,
 });
 
 addons.setConfig({ theme });
